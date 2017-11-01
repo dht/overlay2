74475ff51e8fd5ab56091bd72b80c6bfe36fb6aa
Guy <guyguys@gmail.com>
2017-11-01 14:45:33 +0200

    colors

---
+++ src/constants/colorsEditor.js
@@ -0,0 +1,8 @@
+export default {
+    colorMain: '#5C6E6B',
+    colorSecondary: '#8F8781',
+    colorThird: '#D4CFC5',
+    colorFour: '#C6BFB9',
+    colorDark: '#565656',
+    colorWhite: '#ECE3D9',
+}
